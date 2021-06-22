a188a7544f55daa19e427bad23563a34a5d5ec0a
abdu-zeyad <abd.d@live.com>
2021-06-22 13:43:20 +0300

    finish lab 11

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MyfavBook from "./MyFavoriteBooks";
+import Profile from "./profile";
+import { withAuth0 } from "@auth0/auth0-react";
 
 class App extends React.Component {
   render() {
     console.log("app", this.props);
+    const { isAuthenticated } = this.props.auth0;
+
     return (
       <>
         <Router>
           <Header />
           <Switch>
             <Route exact path="/">
+              {isAuthenticated && <MyfavBook />}
               {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
             </Route>
+            <Profile />
             {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
           </Switch>
           <Footer />
@@ -23,4 +30,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import './header.css';
+import "./header.css";
+import Login from "./Login";
+import LogoutButton from "./LogoutButton";
+import { withAuth0 } from "@auth0/auth0-react";
 
 class Header extends React.Component {
   render() {
-    return(
+    const { isAuthenticated } = this.props.auth0;
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {!isAuthenticated && <Login />}
+        {isAuthenticated && <LogoutButton />}
+        {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
       </Navbar>
-    )
+    );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
